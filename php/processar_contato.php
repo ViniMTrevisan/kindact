d7d67b8cca75beeb6119887044b111e604741f05
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-06-25 19:02:32 -0300

    arrumei mais um lugar que precisava de tb_candidatura

--- php/processar_contato.php
+++ php/processar_contato.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] !== 'ong') {
     header("Location: /kindact/main/login_ong.html");
     exit();
